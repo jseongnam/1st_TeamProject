e648e3c576e3c4a47c711d134668bc253b61cf6b
jseongnam <105405028+jseongnam@users.noreply.github.com>
2022-11-24 17:40:00 +0900

    crawling_2

--- crawling_2.py
+++ crawling_2.py
@@ -15,10 +15,10 @@ options.add_argument('lang=kr_KR')
 driver = webdriver.Chrome('./chromedriver.exe',options=options)
 
 df_titles = pd.DataFrame()
-for i in range(1,2):        #section
+for i in range(0,6):        #section
     titles = []
     for j in range(1,pages[i]):     #page
-        url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=101#&date=%2000:00:00&page={}'.format(j)
+        url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=10{}#&date=%2000:00:00&page={}'.format(i,j)
         driver.get(url)
         time.sleep(0.2)
         for k in range(1,5):        #x_path
